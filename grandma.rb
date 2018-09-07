90775cca3dd44834042d2f7c3b352ee0833e20ad
Beau Jepson <beau.jepson@jepsonconsulting.com>
2018-09-07 19:25:58 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -12,10 +12,5 @@
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
 def speak_to_grandma(sayToGrandma)
-if sayToGrandma == "I LOVE YOU GRANDMA!"
-  puts = "I LOVE YOU TOO PUMPKIN!"
-elsif sayToGrandma == "Hi Nana, how are you?" || sayToGrandma == "Hi!"
-  puts = "HUH?! SPEAK UP, SONNY!"
-else sayToGrandma == "WHAT DID YOU EAT TODAY?" || sayToGrandma == "WHAT?"
-  puts = "NO, NOT SINCE 1938!"
+
 end
