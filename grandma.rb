946706ef10a740fe68aa38513c34450c7c35d6fb
Beau Jepson <beau.jepson@jepsonconsulting.com>
2018-09-07 19:24:00 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -4,10 +4,19 @@
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
 
-# If you shout, she can hear you (or at least she thinks so) 
+# If you shout, she can hear you (or at least she thinks so)
 # and yells back
 
 # NO, NOT SINCE 1938!
 
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
+def speak_to_grandma(sayToGrandma)
+if sayToGrandma == "I LOVE YOU GRANDMA!"
+  grandmaTalk = "I LOVE YOU TOO PUMPKIN!"
+elsif sayToGrandma == "Hi Nana, how are you?" || sayToGrandma == "Hi!"
+  grandmaTalk = "HUH?! SPEAK UP, SONNY!"
+else sayToGrandma == "WHAT DID YOU EAT TODAY?" || sayToGrandma == "WHAT?"
+  grandmaTalk = "NO, NOT SINCE 1938!"
+puts grandmaTalk
+end
