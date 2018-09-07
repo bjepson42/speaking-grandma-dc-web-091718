edb82a940230ed983a8729ecd4efec5f6340af21
Beau Jepson <beau.jepson@jepsonconsulting.com>
2018-09-07 19:39:39 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -16,7 +16,9 @@ if sayToGrandma == "I LOVE YOU GRANDMA!"
   return "I LOVE YOU TOO PUMPKIN!"
 elsif sayToGrandma == "Hi Nana, how are you?" || sayToGrandma == "Hi!"
   return "HUH?! SPEAK UP, SONNY!"
-else sayToGrandma == "WHAT DID YOU EAT TODAY?"
+elsif sayToGrandma == "WHAT DID YOU EAT TODAY?"
  return "NO, NOT SINCE 1938!"
+ else
+   return "DFLDKFJAKL:JF"
  end
 end
