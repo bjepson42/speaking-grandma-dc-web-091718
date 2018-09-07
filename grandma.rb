3c8c2e2263d844f72f65126de62fed845daa439e
Beau Jepson <beau.jepson@jepsonconsulting.com>
2018-09-07 19:30:19 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -14,10 +14,11 @@
 def speak_to_grandma(sayToGrandma)
 #if sayToGrandma == "I LOVE YOU GRANDMA!"
  puts sayToGrandma
-  puts "I LOVE YOU TOO PUMPKIN!"
+  foo = "I LOVE YOU TOO PUMPKIN!"
   return sayToGrandma
 #elsif sayToGrandma == "Hi Nana, how are you?" || sayToGrandma == "Hi!"
 #  puts = "HUH?! SPEAK UP, SONNY!"
 #else sayToGrandma == "WHAT DID YOU EAT TODAY?" || sayToGrandma == "WHAT?"
 #  puts = "NO, NOT SINCE 1938!"
+foo
 end
